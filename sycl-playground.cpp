6dd9e381b9636b15b9a3a654e5cfabb345c8ac75
Alex Baden <alexander.baden@intel.com>
2024-10-17 01:25:37 +0000

    Check for device memory allocation failure

--- sycl-playground.cpp
+++ sycl-playground.cpp
@@ -23,6 +23,10 @@ int main(int argc, char* argv[]) {
 		ptrs[i] = sycl::malloc_device<char>(bytes, q);
 		allocated_mem_size += bytes;
 		std::cout << "Total allocated mem size after iteration " << i << " : " << allocated_mem_size << " (remaining: " << global_mem_size - static_cast<int64_t>(allocated_mem_size) << ")" << std::endl;
+		if (!ptrs[i]) {
+			std::cout << "Failed to allocate device memory!" << std::endl;
+			return 1;
+		}
 		q.memcpy(ptrs[i], host_buf.data(), bytes).wait_and_throw();
 	}
 
